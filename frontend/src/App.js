4630fc6f2032200272d106e8cbe5827df0c8ee35
Michael Rayner <scetils_michael@hotmail.com>
2020-10-18 23:17:09 +1100

    Created Cart Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Footer from "components/Footer";
 import Header from "components/Header";
+
+import CartScreen from "screens/CartScreen";
 import HomeScreen from "screens/HomeScreen";
 import ProductScreen from "screens/ProductScreen";
 
@@ -13,6 +15,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path="/" exact component={HomeScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
