f91acd225352d3d7f5cdc85a67f9488ac3fda9f8
Michael Rayner <scetils_michael@hotmail.com>
2020-11-01 23:03:56 +1100

    Created frontend auth routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,16 @@ import { protect } from "../middleware/authMiddleware.js";
 import {
   authUser,
   getUserProfile,
-  registerUser
+  registerUser,
+  updateUserProfile
 } from "../controllers/userController.js";
 const router = express.Router();
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
